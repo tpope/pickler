23c4ed67b529bbb7610fb06d4c4daeddf949bb1e
Tim Pope <code@tpope.net>
2008-12-04 19:25:30 -0500

    Colorized search results

--- lib/pickler/runner.rb
+++ lib/pickler/runner.rb
@@ -12,6 +12,28 @@ class Pickler
       @tty = $stdout.tty?
     end
 
+    COLORS = {
+      :black   => 0,
+      :red     => 1,
+      :green   => 2,
+      :yellow  => 3,
+      :blue    => 4,
+      :magenta => 5,
+      :cyan    => 6,
+      :white   => 7
+    }
+
+    STATE_COLORS = {
+      nil           => COLORS[:black],
+      "rejected"    => COLORS[:red],
+      "accepted"    => COLORS[:green],
+      "delivered"   => COLORS[:yellow],
+      "unscheduled" => COLORS[:white],
+      "started"     => COLORS[:magenta],
+      "finished"    => COLORS[:cyan],
+      "unstarted"   => COLORS[:blue]
+    }
+
     STATE_SYMBOLS = {
       "unscheduled" => "  ",
       "unstarted"   => ":|",
@@ -22,6 +44,13 @@ class Pickler
       "accepted"    => ":D"
     }
 
+    TYPE_COLORS = {
+      'chore'   => COLORS[:blue],
+      'feature' => COLORS[:magenta],
+      'bug'     => COLORS[:red],
+      'release' => COLORS[:cyan]
+    }
+
     TYPE_SYMBOLS = {
       "feature" => "*",
       "chore"   => "%",
@@ -29,11 +58,25 @@ class Pickler
       "bug"     => "/"
     }
 
+    def color?
+      case pickler.config["color"]
+      when "always" then true
+      when "never"  then false
+      else
+        @tty && RUBY_PLATFORM !~ /mswin|mingw/
+      end
+    end
+
     def puts_summary(story)
       summary = "%6d " % story.id
       type  = story.estimate || TYPE_SYMBOLS[story.story_type]
       state = STATE_SYMBOLS[story.current_state]
-      summary << "#{state} #{type} "
+      if color?
+        summary << "\e[3#{STATE_COLORS[story.current_state]}m#{state}\e[00m "
+        summary << "\e[01;3#{TYPE_COLORS[story.story_type]}m#{type}\e[00m "
+      else
+        summary << "#{state} #{type} "
+      end
       summary << story.name
       puts summary
     end
